3a54bcad6d87bf05ba5c907961df138943d94501
Maksim Kostromin <daggerok@gmail.com>
2016-06-25 14:09:47 +0300

    initial commit: basic example isung npm scripts.

---
+++ README.md
@@ -0,0 +1,5 @@
+# [webpack-playground](https://github.com/daggerok/webpack-playground)
+
+```sh
+npm start
+```
---
+++ src/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mak on 6/25/16.
+ */
+const $ = require('jquery');
+
+$('#app').hide().html('basic jquery app').fadeIn(1000);
