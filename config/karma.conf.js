5f976d5f56fe0c15fade61f1a70e5d9a666a551c
Maksim Kostromin <daggerok@gmail.com>
2016-12-19 00:27:58 +0200

    update ci; add webpack-dev-middleware for ignoring it output; remove old useless tet webpack config for karma

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -51,6 +51,13 @@ module.exports = (config) => {
     webpack: wpConfig,
 
 
+    // removing webpackoutput when karma run tests:
+    webpackMiddleware: {
+      // noInfo: true, // deprecated,
+      stats: 'errors-only',
+    },
+
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
