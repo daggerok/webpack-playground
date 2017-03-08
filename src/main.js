627794c6b5b728138e30503a8a3e3262c156f54e
Maksim Kostromin <daggerok@gmail.com>
2017-03-09 01:33:12 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -5,27 +5,34 @@ import './main.styl';
 
 const webpack2Url = 'https://webpack.js.org/';
 const target = document.querySelector('.app');
+
 // const app = (props) => (
 //   <a target='_blank' href={webpack2Url}>meet webpack 2</a>
 // );
 //
 // render(React.createElement(app), target);
 
+const paddingStyles = {
+  padding: '1%',
+};
+
 render(
-  <a
-    href={webpack2Url}
-    target='_blank'
-    onClick={e => {
-      e.preventDefault();
-      System.import('./modules/some-module').then(SomeModule => {
-        console.log(`received module: ${JSON.stringify(SomeModule.db)}`);
-        SomeModule.default('ES6 modules success!'); // because of export default ...
-        console.log(`module ${SomeModule.name} is updated`);
-      });
-    }}>
+  <div style={paddingStyles}>
+    <a
+      href={webpack2Url}
+      target='_blank'
+      onClick={e => {
+        e.preventDefault();
+        System.import('./modules/some-module').then(SomeModule => {
+          console.log(`received module: ${JSON.stringify(SomeModule.db)}`);
+          SomeModule.default('ES6 modules success!'); // because of export default ...
+          console.log(`module ${SomeModule.name} is updated`);
+        });
+      }}>
       <div>meet webpack 2!</div>
       <div>
         open developer tool, network and click to test lazy loading using ES6 modules
       </div>
-    </a>,
+    </a>
+  </div>,
   target);
